28e2cc31cf375f52aa1432ce19f10ac9ba6bae57
bh1044501616 <1044501616@qq.com>
2018-08-23 16:16:01 +0800

    修复下载链接存储错误bug

--- src/main/java/org/iqalliance/smallProject/common/web/StaticValue.java
+++ src/main/java/org/iqalliance/smallProject/common/web/StaticValue.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class StaticValue {
 //	public static final String URL = "http://localhost:8080/smallProject1.0/";
-	public static final String URL = "http://www.psnhub.club/wx/";
+	public static final String URL = "https://www.psnhub.club/wx/";
 	public static final String FILE_PATH = "/home/tomcat/wx_file/";
 }
